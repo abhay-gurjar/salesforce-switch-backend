85913ce0c191b51c8391e47b4b7d6fdc36f6b019
abhay-gurjar <abhaybougha@gmail.com>
2025-12-28 13:23:21 +0530

    abhay

--- app.js
+++ app.js
@@ -3,18 +3,23 @@ const cors = require("cors");
 require("dotenv").config();
 
 const salesforceRoutes = require("./src/routes/salesforceRoutes");
-const { callback } = require("./src/controllers/salesforceController");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: true,
+    methods: ["GET", "POST", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Salesforce Switch Backend is running");
 });
-
-app.get("/oauth/callback", callback);
 
 app.use("/api", salesforceRoutes);
 
